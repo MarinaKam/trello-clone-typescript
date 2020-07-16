ad396715522aabec02d86f1d6358761a4088de78
Marina <marina.stozhok@gmail.com>
2020-07-16 21:06:48 +0300

    feature: Added the ability to move cards, improved code style in app provider.

--- src/AppProvider/reducer.tsx
+++ src/AppProvider/reducer.tsx
@@ -7,28 +7,37 @@ import { DragItem } from '../DragItem';
 
 export type Action =
   | {
-      type: types.ADD_LIST,
-      payload: string
-    }
+    type: types.ADD_LIST,
+    payload: string
+  }
   | {
-      type: types.ADD_TASK,
-      payload: { text: string; taskId: string }
-    }
+    type: types.ADD_TASK,
+    payload: { text: string; taskId: string }
+  }
   | {
-      type: types.MOVE_LIST,
-      payload: {
-        dragIndex: number,
-        hoverIndex: number
-      }
+    type: types.MOVE_LIST,
+    payload: {
+      dragIndex: number,
+      hoverIndex: number
     }
+  }
   | {
-      type: types.SET_DRAGGED_ITEM,
-      payload: DragItem | undefined
-    };
+    type: types.MOVE_TASK,
+    payload: {
+      dragIndex: number,
+      hoverIndex: number,
+      sourceColumn: string,
+      targetColumn: string
+    }
+  }
+  | {
+    type: types.SET_DRAGGED_ITEM,
+    payload: DragItem | undefined
+  };
 
 export const reducer = (state: AppState, action: Action) => {
   switch (action.type) {
-    case types.ADD_LIST:
+    case types.ADD_LIST: {
       return {
         ...state,
         lists: [
@@ -36,8 +45,9 @@ export const reducer = (state: AppState, action: Action) => {
           { id: uuidv4(), text: action.payload, tasks: []}
         ]
       };
+    }
 
-    case types.ADD_TASK:
+    case types.ADD_TASK: {
       const index = findItemById(state.lists, action.payload.taskId);
 
       state.lists[index].tasks.push({
@@ -46,21 +56,36 @@ export const reducer = (state: AppState, action: Action) => {
       });
 
       return { ...state };
+    }
 
-    case types.MOVE_LIST:
+    case types.MOVE_LIST: {
       const { dragIndex, hoverIndex } = action.payload;
 
       state.lists = moveItem(state.lists, dragIndex, hoverIndex);
 
       return { ...state };
+    }
 
-    case types.SET_DRAGGED_ITEM:
+    case types.SET_DRAGGED_ITEM: {
       return {
         ...state,
         draggedItem: action.payload
-      }
+      };
+    }
 
-    default:
+    case types.MOVE_TASK: {
+      const { dragIndex, hoverIndex, sourceColumn, targetColumn } = action.payload;
+      const sourceLaneIndex = findItemById(state.lists, sourceColumn);
+      const targetLaneIndex = findItemById(state.lists, targetColumn);
+      const item = state.lists[sourceLaneIndex].tasks.splice(dragIndex, 1)[0];
+
+      state.lists[targetLaneIndex].tasks.splice(hoverIndex, 0, item);
+
+      return { ...state };
+    }
+
+    default: {
       return state;
+    }
   }
 };
--- src/Column/Card/Card.tsx
+++ src/Column/Card/Card.tsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useDrop } from 'react-dnd';
+import * as types from '../../DragItem/types';
 import { CardContainer } from './styles';
+import { CardDragItem, useItemDrag } from '../../DragItem';
+import { useAppState } from '../../AppProvider';
+import { isHidden } from '../../helpers';
 
 interface CardProps {
-  text: string
+  text: string,
+  id: string,
+  index: number,
+  columnId: string,
+  isPreview?: boolean
 }
-export const Card = ({ text }: CardProps) => {
+export const Card = ({
+  text,
+  id,
+  index,
+  columnId,
+  isPreview
+}: CardProps) => {
+  const { state, moveTask } = useAppState();
+  const ref = useRef<HTMLDivElement>(null);
+  const { drag } = useItemDrag({ type: types.CARD, text, id, index, columnId });
+  const [, drop] = useDrop({
+    accept: types.CARD,
+    hover(item: CardDragItem) {
+      if (item.type === types.CARD) {
+        if (item.id === id) {
+          return;
+        }
+
+        const dragIndex = item.index;
+        const hoverIndex = index;
+        const sourceColumn = item.columnId;
+        const targetColumn = columnId;
+
+        moveTask({ hoverIndex, dragIndex, sourceColumn, targetColumn });
+        item.index = hoverIndex;
+        item.columnId = targetColumn;
+      }
+    }
+  });
+
+  useEffect(() => {
+    drag(drop(ref));
+  }, [ drag, drop ]);
+
   return (
-    <CardContainer>{text}</CardContainer>
+    <CardContainer
+      isHidden={isHidden(isPreview, state.draggedItem, types.CARD, id)}
+      isPreview={isPreview}
+      ref={ref}
+    >
+      {text}
+    </CardContainer>
   );
 };
